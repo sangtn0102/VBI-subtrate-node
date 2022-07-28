fb4816e98c20c14efa947dcff7a815e32b26e855
Trần Nhật Sang <sangtn0102@gmail.com>
2022-07-28 10:49:35 +0200

    Benchmark extrinsic transfer in pallet-kitties

--- pallets/kitties/src/benchmarking.rs
+++ pallets/kitties/src/benchmarking.rs
@@ -1,6 +1,7 @@
 //! Benchmarking setup for pallet-template
 
 use super::*;
+use frame_benchmarking::account;
 
 #[allow(unused)]
 use crate::Pallet as Kitties;
@@ -20,6 +21,22 @@ benchmarks! {
 	// kiểm tra lại trạng thái storage khi thực hiện extrinsic xem đúng chưa 
 	verify {
 		assert_eq!(KittyId::<T>::get(), 1);
+	}
+
+	//benchmark transfer
+	transfer {
+		let caller: T::AccountId = whitelisted_caller();
+		let bob: T::AccountId = account("bob", 0, 0);
+		let dna : Vec<u8> = b"lienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlqlienlq".to_vec();
+		Kitties::<T>::create_kitty(RawOrigin::Signed(caller.clone()).into(), dna)?;
+
+		let dna_of_kitty = KittiesOwned::<T>::get(&caller)[0];
+
+
+	}: transfer(RawOrigin::Signed(caller.clone()), bob.clone(), dna_of_kitty)
+
+	verify {
+		assert_eq!(KittiesOwned::<T>::get(&bob)[0], dna_of_kitty);
 	}
  
 	// thực hiện benchmark với mock runtime, storage ban đầu.
